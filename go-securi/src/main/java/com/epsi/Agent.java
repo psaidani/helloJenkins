f9a6645d78f52a40d3b3fcb036dc68afebbe6d83
lucasvavon <lucasvavon@icloud.com>
2021-11-05 16:21:35 +0100

    commit sur dev 5 - ajout txt et methode reader

---
+++ go-securi/src/main/java/com/epsi/Agent.java
@@ -0,0 +1,5 @@
+package com.epsi;
+
+public class Agent {
+    
+}
--- go-securi/src/main/java/com/epsi/App.java
+++ go-securi/src/main/java/com/epsi/App.java
@@ -1,5 +1,14 @@
 package com.epsi;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 /**
  * Hello world!
  *
@@ -8,6 +17,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - mspr" );
+        
+        try
+    {
+      // Le fichier d'entrée
+      File file = new File("cberthier.txt");    
+      // Créer l'objet File Reader
+      FileReader fr = new FileReader(file);  
+      // Créer l'objet BufferedReader        
+      BufferedReader br = new BufferedReader(fr);  
+      StringBuffer sb = new StringBuffer();    
+      String line;
+      while((line = br.readLine()) != null)
+      {
+        // ajoute la ligne au buffer
+        sb.append(line);      
+        sb.append("\n");     
+      }
+      fr.close();    
+      System.out.println("Contenu du fichier: ");
+      System.out.println(sb.toString());  
+    }
+    catch(IOException e)
+    {
+      e.printStackTrace();
+    }
     }
 }
