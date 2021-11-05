6cf48775e617cac0789d48c002ee4038153442b4
lucasvavon <lucasvavon@icloud.com>
2021-11-05 11:53:54 +0100

    commit initial

---
+++ go-securi/src/main/java/com/epsi/App.java
@@ -0,0 +1,13 @@
+package com.epsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - mspr" );
+    }
+}
