2fec4fb98ce06007b8d032fd64b13a743eed7270
lucasvavon <lucasvavon@icloud.com>
2022-02-23 14:35:59 +0100

    maj fichiers .txt

--- go-securi/src/main/java/com/epsi/App.java
+++ go-securi/src/main/java/com/epsi/App.java
@@ -1,53 +1,19 @@
 package com.epsi;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
-import java.util.ArrayList;
 
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    static ArrayList<String> result = new ArrayList<>();
     public static void main( String[] args ) throws IOException
     {
+        Fichiers f = new Fichiers();
+        f.readStaff();
+        f.readAgent();
+
+        f.writeIndex();
+        f.writeAgent();
         
-        try
-        {
-        // Le fichier d'entrée
-        File file = new File("cberthier.txt");    
-        // Créer l'objet File Reader
-        FileReader fr = new FileReader(file);  
-        // Créer l'objet BufferedReader        
-        BufferedReader br = new BufferedReader(fr);  
-        StringBuffer sb = new StringBuffer();    
-        String line;
-            while((line = br.readLine()) != null)
-            {
-                // ajoute la ligne au buffer
-                sb.append(line);      
-                sb.append("\n");
-            }
-        fr.close();    
-        System.out.println("Contenu du fichier: ");
-        System.out.println(sb.toString());  
-        }
-        catch(IOException e)
-        {
-
-        File outputFile = new File("output.txt");
-        BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
-
-
-      e.printStackTrace();
-    }
     }
 }
